b220a4178eec9594b923e5ad1e3323351e685a49
Tarcizio Santos <tarciziosantos.dev@gmail.com>
2025-02-27 23:32:51 -0300

    feat: implement VatTax class for VAT calculation

---
+++ compartamentais/strategy/calculo_imposto/tax_types/vat_tax.py
@@ -0,0 +1,7 @@
+from calculo_imposto import TaxStrategy
+
+class VatTax(TaxStrategy):
+    def calculate_tax(self, value: float) -> float:
+        return {
+            'tax': value * 0.20
+        }
